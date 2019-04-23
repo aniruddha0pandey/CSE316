89d7be517b4561a2b2efaf1f255011e2dbc8262a
Aniruddha Pandey <anirudh.pandev@gmail.com>
2019-04-24 00:23:37 +0530

    update

--- converter.py
+++ converter.py
@@ -9,7 +9,7 @@ from pdf2image.exceptions import (
 images = convert_from_path(
 		 	'./ListOfQuestions_K17BN.pdf',
 		 	output_folder="./snaps/",
-		 	first_page=7,
-		 	last_page=8,
+		 	first_page=14,
+		 	last_page=14,
 		 	fmt='png'
 	)
